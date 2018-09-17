e1e9f9b1fabae07a0c3c8dd68ffa6f0517ee1040
keiko <keikomakiba@gmail.com>
2018-09-17 04:20:37 +0000

    SENDGRID

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,6 +57,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
-  config.action_mailer.default_url_options = { host: 'hidden-depths-80971.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'gigbuddy-keiko5645.c9users.io' }
   config.action_mailer.delivery_method = :letter_opener
 end
