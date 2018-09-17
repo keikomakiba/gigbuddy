681aad75f843302e387c772ff52cbe8028b1c32d
keiko <keikomakiba@gmail.com>
2018-09-17 09:08:15 +0000

    change application.coll

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,12 +16,12 @@ class ApplicationController < ActionController::Base
   
   helper_method :current_user, :logged_in?
 
-  private
-    def current_user
-      @current_user ||= User.find_by_id(session[:user])
-    end
+  # private
+  #   def current_user
+  #     @current_user ||= User.find_by_id(session[:user])
+  #   end
 
-    def logged_in?
-      current_user != nil
-    end
+  #   def logged_in?
+  #     current_user != nil
+  #   end
 end
