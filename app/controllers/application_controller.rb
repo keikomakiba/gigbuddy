b671a9e40afc04d9c42340d04ee4c50156bf4b3e
keiko <keikomakiba@gmail.com>
2018-09-17 08:00:34 +0000

    error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,26 +2,26 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   
-  # rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  # rescue_from ActionController::RoutingError, with: :render_404
-  # rescue_from Exception, with: :render_500
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from Exception, with: :render_500
 
-  # def render_404
-  # render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
-  # end
+  def render_404
+  render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
+  end
 
-  # def render_500
-  # render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
-  # end
+  def render_500
+  render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
+  end
   
-  # helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?
 
-  # private
-  #   def current_user
-  #     @current_user ||= User.find_by_id(session[:user])
-  #   end
+  private
+    def current_user
+      @current_user ||= User.find_by_id(session[:user])
+    end
 
-  #   def logged_in?
-  #     current_user != nil
-  #   end
+    def logged_in?
+      current_user != nil
+    end
 end
